3605d37d34f004ea7850e80b5b67c8a51a5727df
PranavkaushikDhara <47033450+PranavkaushikDhara@users.noreply.github.com>
2023-12-28 13:26:59 +0530

    Removed speed-insights

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,11 +9,9 @@ import CustomTabPanel from './CustomTabPanel';
 import Education from './Education';
 import Footer from './Footer';
 import OtherSkills from './OtherSkills';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 const Home = () => {
   return (
     <Container>
-      <SpeedInsights/>
       <Sidebar>
         <Sidenav />
       </Sidebar>
